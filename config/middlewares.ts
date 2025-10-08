5dcfd3ebb56e5dfa9163c8bd18653adaf68db912
fabianealmiron <fabian.e.almiron@gmail.com>
2025-10-08 16:45:02 -0600

    Fix CORS: Allow Vercel frontend domain

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,8 +5,14 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000', 'http://localhost:3001'],
+      origin: [
+        'http://localhost:3000', 
+        'http://localhost:3001',
+        'https://the-pp-new.vercel.app',
+        'https://*.vercel.app'
+      ],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true,
     },
   },
   'strapi::poweredBy',
